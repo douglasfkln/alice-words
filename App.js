7dad8b7a7a2d074df282359ba2ef74e3b64e060e
Douglas Franklin <douglasfkln@outlook.com>
2019-03-16 14:36:30 -0300

    Set storage and states

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 
 import Logo from './components/Logo';
 import Loader from './components/Loader';
@@ -14,28 +14,58 @@ export default class App extends React.Component {
 
   // Tem que ser state
   state = {
+    minutesToWait: 0, // tempo ate o usuario informar as palavras
+    minutesToShow: 0, // representa quanto tempo falta para exibir as palavras
+    showLogo: true,
+    showLoader: true,
+    showReadWords: false,
+    showTimeCounter: false,
+    showInputWords: false,
+    showResults: false,
+    palavra1: '',
+    palavra2: '',
+    palavra3: '',
+    results: ''
+  }
 
+  changeState = (newState) => this.setState(newState);
+
+  storeData = async (data) => {
+    try {
+      await AsyncStorage.setItem('ALICEWORDS', JSON.stringify(data));
+    } catch (error) {
+      console.error("Erro de storeData", error);
+    }
+  }
+
+  getData = async () => {
+    try {
+      let value = await AsyncStorage.getItem('ALICEWORDS');
+      return (value != null) ? JSON.parse(value) : false;
+    } catch (error) {
+      console.error("Erro get Storage", error);
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <If test={false}>
+        <If test={this.state.showLogo}>
           <Logo />
         </If>
-        <If test={false}>
+        <If test={this.state.showLoader}>
           <Loader />
         </If>
-        <If test={false}>
+        <If test={this.state.showReadWords}>
           <ReadWords />
         </If>
-        <If test={false}>
+        <If test={this.state.showTimeCounter}>
           <TimeCounter />
         </If>
-        <If test={false}>
+        <If test={this.state.showInputWords}>
           <InputWords />
         </If>
-        <If test={false}>
+        <If test={this.state.showResults}>
           <Results />
         </If>
       </View>
